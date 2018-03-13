eaa5f4f782ea40081b9ad4dab94138bcfabc569d
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-03-13 20:01:47 +0100

    (creation currency-form) style: refactor model-driven form by reactive Form
    Change approach of the form for more flexibility
    
     Changes to be committed:
    	modified:   src/app/Feature/Currency/create/Currency-Form/currency-form.component.html
    	modified:   src/app/Feature/Currency/create/Currency-Form/currency-form.component.ts
    	modified:   src/app/Shared/shared-module.ts

--- src/app/Feature/Currency/create/Currency-Form/currency-form.component.ts
+++ src/app/Feature/Currency/create/Currency-Form/currency-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, ViewChild, Output, EventEmitter, OnInit } from '@angular/core';
 import { AppConstant } from '@dsc4u/Shared/Constant/app-constant';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators, AbstractControl} from '@angular/forms';
+
 
 @Component({
     selector: 'dcs4u-currency-creation-currency-form',
@@ -8,20 +9,49 @@ import { NgForm } from '@angular/forms';
     styleUrls: ['./currency-form.component.scss']
 })
 
-export class CurrencyFormComponent {
+export class CurrencyFormComponent implements OnInit {
 
     constructor() { }
 
-    public appConstant = AppConstant;
-    @ViewChild ('currencyForm') currencyForm: NgForm;
     @Output() validityFormEmitter: EventEmitter<any> = new EventEmitter<any>();
+    public appConstant = AppConstant;
+    private _currencyForm: FormGroup;
+
+    /**
+     * @name CurrencyFormComponent#ngOnInit
+     * @type {function}
+     * @description Lifecycle Angular
+     */
+    ngOnInit(): void {
+      this.createCurrencyForm();
+    }
 
     /**
      * @name CurrencyFormComponent#emitValidityForm
      * @description Emit an event with the valididty of the form and the value of the form
      */
     emitValidityForm(): void {
-        const objectToEmit = { name: this.currencyForm.form.value.currencyName, valid: this.currencyForm.form.valid };
+        const objectToEmit = { name: this._currencyForm.value.currencyName, valid: this._currencyForm.valid };
         this.validityFormEmitter.emit(objectToEmit);
+    }
+
+    /**
+     * @name CurrencyFormComponent#createCurrencyForm
+     * @type {function}
+     * @description create the form for currency
+     */
+    createCurrencyForm() {
+      this._currencyForm = new FormGroup({
+        currencyName:  new FormControl('', Validators.required)
+      });
+    }
+
+    /**
+     * @name CurrencyFormComponent#currencyForm
+     * @type {function}
+     * @description getter of currency Form
+     */
+    public get currencyForm(): FormGroup {
+      return this._currencyForm;
     }
 }
--- src/app/Shared/shared-module.ts
+++ src/app/Shared/shared-module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AngularMaterialModule } from '@dsc4u/Shared/Angular-Material/angular-material.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-    imports: [ AngularMaterialModule, FormsModule, CommonModule],
-    exports: [ AngularMaterialModule, FormsModule, CommonModule],
+    imports: [ AngularMaterialModule, FormsModule, CommonModule, ReactiveFormsModule],
+    exports: [ AngularMaterialModule, FormsModule, CommonModule, ReactiveFormsModule],
     declarations: [],
     providers: [],
 })
