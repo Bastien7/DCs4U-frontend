16531654da1116406a6928f01619570ae424736e
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-03-26 11:51:06 +0200

    (page loading) feat: add interceptor http request for loading
    add an intercepter http request for do a loading for each call
    
     Changes to be committed:
    	modified:   src/app/Feature/Currency/currency-routing.module.ts
    	new file:   src/app/Shared/Service/loading.service.ts

--- src/app/Feature/Currency/currency-routing.module.ts
+++ src/app/Feature/Currency/currency-routing.module.ts
@@ -4,16 +4,18 @@ import { CurrencyComponent } from '@dsc4u/Feature/Currency/currency.component';
 import { CreateCurrencyComponent } from '@dsc4u/Feature/Currency/create/create-currency.component';
 
 const currencyRoute: Routes = [
-    { path: '', component: CurrencyComponent, pathMatch: 'prefix', children: [
-        { path: '', redirectTo: '/currency/create', pathMatch: 'full' },
-        { path: 'create', component: CreateCurrencyComponent  }
-    ]}
+  {
+    path: '', component: CurrencyComponent, pathMatch: 'prefix', children: [
+      { path: '', redirectTo: '/currency/create', pathMatch: 'full' },
+      { path: 'create', component: CreateCurrencyComponent }
+    ]
+  }
 ];
 
 @NgModule({
-    imports: [ RouterModule.forChild(currencyRoute)],
-    exports: [ RouterModule ],
-    declarations: [],
-    providers: [],
+  imports: [RouterModule.forChild(currencyRoute)],
+  exports: [RouterModule],
+  declarations: [],
+  providers: [],
 })
 export class CurrencyRoutingModule { }
