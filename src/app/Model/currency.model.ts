2faec9557cdb053a4d26e1708482c9a73aa4f8f7
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-04-03 13:52:10 +0200

    (model currency) refactor: fix concordance model with back-end
     Changes to be committed:
    	modified:   src/app/Model/currency.model.ts

--- src/app/Model/currency.model.ts
+++ src/app/Model/currency.model.ts
@@ -3,6 +3,8 @@ import { Owner } from '@dcs4u/Model/owner.model';
 export class Currency {
   constructor(
     private _id?: string,
+    private name?: string,
+    private symbol?: string,
     private owner?: Owner,
   ) {}
 }
