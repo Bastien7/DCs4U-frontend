fa7ceddaac52e45d2315c4289067760ef07e4616
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-04-06 14:39:10 +0200

    (page menu-bar) feat: create menu-bar component
     Changes to be committed:
    	new file:   src/app/Core/Menu-Bar/menu-bar.component.html
    	new file:   src/app/Core/Menu-Bar/menu-bar.component.scss
    	new file:   src/app/Core/Menu-Bar/menu-bar.component.ts
    	modified:   src/app/Core/core.module.ts
    	modified:   src/app/app.component.html
    	modified:   src/app/app.component.scss

---
+++ src/app/Core/Menu-Bar/menu-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dcs4u-menu-bar',
+  templateUrl: 'menu-bar.component.html',
+  styleUrls: ['menu-bar.component.scss']
+})
+
+export class MenuBarComponent {
+  constructor() { }
+}
--- src/app/Core/core.module.ts
+++ src/app/Core/core.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material';
 import { HeaderComponent } from '@dsc4u/Core/Header/header.component';
 import { LoaderComponent } from '@dsc4u/Core/Loader/loader.component';
+import { MenuBarComponent } from '@dsc4u/Core/Menu-Bar/menu-bar.component';
 
 @NgModule({
     imports: [ CommonModule, MatProgressSpinnerModule ],
-    exports: [ HeaderComponent, LoaderComponent ],
-    declarations: [ HeaderComponent, LoaderComponent ],
+    exports: [ HeaderComponent, LoaderComponent, MenuBarComponent ],
+    declarations: [ HeaderComponent, LoaderComponent, MenuBarComponent ],
     providers: [],
 })
 export class CoreModule { }
